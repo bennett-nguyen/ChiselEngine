8eb496d9536e0c6fa99e6fcb6477fed1b47b1f39
Bennett Nguyen <83117848+bennett-nguyen@users.noreply.github.com>
2025-06-22 19:45:22 +0700

    Update constant.hpp

--- include/constant.hpp
+++ include/constant.hpp
@@ -9,16 +9,10 @@ namespace Constant {
     constexpr unsigned CHUNK_HEIGHT = 50;
     constexpr unsigned CHUNK_AREA = CHUNK_SIZE * CHUNK_SIZE;
     constexpr unsigned CHUNK_VOLUME = CHUNK_AREA * CHUNK_HEIGHT;
-    constexpr unsigned LOAD_DISTANCE = 12;
+    constexpr unsigned LOAD_DISTANCE = 15;
 
-    constexpr float PLAYER_HEIGHT = 1.5f;
-    constexpr float PLAYER_SPEED = 30.0f;
-    constexpr float FOV = 60.0f; // in degrees, CONVERT TO RADIANS BEFORE USING!
-    constexpr float GRAVITATIONAL_ACCELERATION = 0.3f;
-    constexpr float JUMP_VELOCITY = 2.0f;
-    constexpr unsigned MAX_VOXEL_TRAVERSED = 8;
     constexpr float MAX_RAY_LENGTH = 8.78f;
-    constexpr float CUBE_MESH_SCALE = 1.0f / 256.0f;
+    constexpr unsigned MAX_VOXEL_TRAVERSED = 8;
 }
 
 #endif
