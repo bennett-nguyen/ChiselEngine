5b4c42635e7b1199730674e598cec6f013fa3cde
Bennett Nguyen <83117848+bennett-nguyen@users.noreply.github.com>
2025-07-25 19:07:07 +0700

    Increase loading distance

--- include/constant.hpp
+++ include/constant.hpp
@@ -11,7 +11,9 @@ namespace Constant {
     constexpr unsigned CHUNK_HEIGHT = 32;
     constexpr unsigned CHUNK_AREA = CHUNK_SIZE * CHUNK_SIZE;
     constexpr unsigned CHUNK_VOLUME = CHUNK_AREA * CHUNK_HEIGHT;
-    constexpr unsigned LOAD_DISTANCE = 15;
+
+    // for the sake of your computer, don't put it above 16
+    constexpr unsigned LOAD_DISTANCE = 16;
 
     constexpr float MAX_RAY_LENGTH = 8.78f;
     constexpr unsigned MAX_VOXEL_TRAVERSED = 8;
